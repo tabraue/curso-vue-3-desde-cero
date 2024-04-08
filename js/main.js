d2d3418297a7ae2b01386a53f228df8377bb0c4d
Diana Tabraue <dianatabraue@gmaill.com>
2024-04-08 16:58:11 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-const API = "https://api.github.com/users/";
+const API = 'https://api.github.com/users/';
+
+const app = Vue.createApp({
+  data() {
+    return {
+      search: null,
+      result: null,
+      error: null,
+    };
+  },
+  methods: {
+    async doSearch() {
+        this.result = this.error = null
+      try {
+        const res = await fetch(`${API}${this.search}`); // ||user de parametros
+        if (!res.ok) throw new Error('User not found')
+
+        const data = await res.json();
+
+        console.log(data);
+        this.result = data;
+
+      } catch (error) {
+        console.log(error);
+        this.error = error; // error propiamente dicho
+
+      } finally {
+        //this.search = true;
+      }
+    },
+  },
+});
